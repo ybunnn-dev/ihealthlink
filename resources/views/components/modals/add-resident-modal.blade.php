4f28dea1b8513ebe9a07798ab79b210f715c924e
John Ivan Belaro <belaroivan17@gmail.com>
2025-08-07 17:04:24 +0800

    initialized part 5 of add resident modal

--- resources/views/components/modals/add-resident-modal.blade.php
+++ resources/views/components/modals/add-resident-modal.blade.php
@@ -35,13 +35,13 @@
                         <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
                             <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
                                 <span class="me-2 bg-gray-100 p-4">4</span>
-                                <span class="hidden">Assess for Emergency Indicators</span>
+                                <span class="hidden">Family History</span>
                             </span>
                         </li>
                         <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
                             <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
                                 <span class="me-2 bg-gray-100 p-4">5</span>
-                                <span class="hidden">Assess for Emergency Indicators</span>
+                                <span class="hidden">NCD Risk Factors</span>
                             </span>
                         </li>
                         <li class="flex items-center">
