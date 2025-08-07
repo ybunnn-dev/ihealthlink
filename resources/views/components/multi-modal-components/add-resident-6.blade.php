5030ddc14ae0aa30de808385c55b20d634859440
John Ivan Belaro <belaroivan17@gmail.com>
2025-08-08 00:46:16 +0800

    interface completed

--- resources/views/components/multi-modal-components/add-resident-6.blade.php
+++ resources/views/components/multi-modal-components/add-resident-6.blade.php
@@ -1,9 +1,9 @@
 <div class="grid grid-cols-1 gap-6">
-   <div class="grid grid-cols-7 items-center">
+    <div class="grid grid-cols-7 items-center">
         <p class="col-span-1 font-semibold text-main_font">Blood Sugar</p>
         <hr class="col-span-6 border-t border-t-[0.5px] border-main_font">
-   </div>
-   <div class="grid grid-cols-1 slg:grid-cols-3 gap-4">
+    </div>
+    <div class="grid grid-cols-1 slg:grid-cols-3 gap-4">
         <div class="grid grid-cols-1 relative mb-3">
             <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">FBS Result</label>
             <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
@@ -16,27 +16,28 @@
             <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">Date Taken</label>
             <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
         </div>
-   </div>
-   <p class="col-span-1 text-sm text-main_font">Check if DM clinical symptoms are present</p>
-   <div class="grid grid-cols-1 slg:grid-cols-3 gap-6 col-span-1">
+    </div>
+    <p class="col-span-1 text-sm text-main_font">Check if DM clinical symptoms are present</p>
+    <div class="grid grid-cols-1 slg:grid-cols-3 gap-6 col-span-1 mb-3">
         <div class="flex items-center mb-6 col-span-1">
             <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Polyphagia</label>
+            <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Polyphagia</label>
         </div>
         <div class="flex items-center mb-6 col-span-1">
             <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Polydipsia</label>
+            <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Polydipsia</label>
         </div>
         <div class="flex items-center mb-6 col-span-1">
             <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Polyuria</label>
+            <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Polyuria</label>
         </div>
-   </div>
+    </div>
+
     <div class="grid grid-cols-7 items-center">
         <p class="col-span-1 font-semibold text-main_font">Lipid Profile</p>
         <hr class="col-span-6 border-t border-t-[0.5px] border-main_font">
-   </div>
-   <div class="grid grid-cols-1 slg:grid-cols-3 gap-4">
+    </div>
+    <div class="grid grid-cols-1 slg:grid-cols-3 gap-4">
         <div class="grid grid-cols-1 relative mb-3">
             <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">Total Cholesterol</label>
             <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
@@ -49,8 +50,8 @@
             <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">LDL</label>
             <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
         </div>
-   </div>
-   <div class="grid grid-cols-1 slg:grid-cols-3 gap-4">
+    </div>
+    <div class="grid grid-cols-1 slg:grid-cols-3 gap-4 mb-3">
         <div class="grid grid-cols-1 relative mb-3">
             <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">VLDL</label>
             <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
@@ -63,7 +64,56 @@
             <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">Date Taken</label>
             <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
         </div>
-   </div>
-   
+    </div>
+
+    <div class="grid grid-cols-7 items-center">
+        <p class="col-span-1 font-semibold text-main_font">Urinalysis</p>
+        <hr class="col-span-6 border-t border-t-[0.5px] border-main_font">
+    </div>
+
+    <div class="grid grid-cols-1 slg:grid-cols-3 gap-4 mb-3">
+        <div class="grid grid-cols-1 relative mb-3">
+            <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">Protein</label>
+            <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
+        </div>
+        <div class="grid grid-cols-1 relative mb-3">
+            <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">Ketones</label>
+            <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
+        </div>
+        <div class="grid grid-cols-1 relative mb-3">
+            <label for="addResBMI" class="text-sm font-medium text-main_font col-span-1">Date Taken</label>
+            <input type="text" id="addResBMI" class="border border-gray-300 text-gray-700 rounded-lg p-2">
+        </div>
+    </div>
+
+    <div class="grid grid-cols-7 items-center">
+        <p class="col-span-1 font-semibold text-main_font">COPD</p>
+        <hr class="col-span-6 border-t border-t-[0.5px] border-main_font">
+    </div>
+
+    <div class="grid grid-cols-1 slg:grid-cols-3 gap-3">
+        <div class="grid grid-cols-1 gap-3 col-span-1">
+            <div class="flex items-center mb-6">
+                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Breathlessness</label>
+            </div>
+
+            <div class="flex items-center mb-6">
+                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Chronic Cough</label>
+            </div>
+        </div>
+        <div class="grid grid-cols-1 gap-3 col-span-1">
+            <div class="flex items-center mb-6">
+                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Sputum (Mucous) Production</label>
+            </div>
+
+            <div class="flex items-center mb-6">
+                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <label for="default-checkbox" class="ms-2 text-sm font-medium text-normal_font">Wheezing</label>
+            </div>
+        </div>
+    </div>
 </div>
 
