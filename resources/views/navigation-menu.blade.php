e16de2ea1199da35b97f866775210a06de07cc7f
John Ivan Belaro <belaroivan17@gmail.com>
2025-08-09 11:18:44 +0800

    added default icon on profile on navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -84,16 +84,25 @@
                             @else
                                <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex flex-col items-start px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                        <div class="flex items-center gap-6">
-                                            <img class="justify-start size-8 rounded-full object-cover" src="{{ asset(Auth::user()->profile_photo_url) }}" alt="{{ Auth::user()->name }}">
-                                            <div class="flex-col items-center">
-                                                <div class="flex items-center">
-                                                    <p class="text-semibold text-main_font">{{ Auth::user()->name }}</p>
+                                        <div class="flex items-center gap-3">
+                                            <svg class="justify-start size-9 rounded-full object-cover" 
+                                                viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <path opacity="0.4" d="M12.1207 12.78C12.0507 12.77 11.9607 12.77 11.8807 12.78C10.1207 12.72 8.7207 11.28 8.7207 9.50998C8.7207 7.69998 10.1807 6.22998 12.0007 6.22998C13.8107 6.22998 15.2807 7.69998 15.2807 9.50998C15.2707 11.28 13.8807 12.72 12.1207 12.78Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                    <path opacity="0.34" d="M18.7398 19.3801C16.9598 21.0101 14.5998 22.0001 11.9998 22.0001C9.39977 22.0001 7.03977 21.0101 5.25977 19.3801C5.35977 18.4401 5.95977 17.5201 7.02977 16.8001C9.76977 14.9801 14.2498 14.9801 16.9698 16.8001C18.0398 17.5201 18.6398 18.4401 18.7398 19.3801Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                </g>
+                                            </svg>
+                                            <div class="flex flex-col items-start justify-start">
+                                                <div class="flex items-center gap-6">
+                                                    <p class="font-semibold text-main_font">{{ Auth::user()->name }}</p>
                                                     <svg class="ms-2 -me-0.5 size-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                                     </svg>
                                                 </div>
-                                                <span class="text-left text-xs font-normal mt-0.5 text-normal_font">MHO Admin</span>
+                                                <span class="text-xs font-normal mt-0.5 text-normal_font">MHO Admin</span>
                                             </div>
                                         </div>
                                     </button>
@@ -108,7 +117,7 @@
                             </div>
 
                             <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('Account Settings') }}
                             </x-dropdown-link>
                                 
                             <x-dropdown-link href="{{ route('midwife.faqs') }}">
