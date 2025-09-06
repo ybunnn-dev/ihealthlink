3b4425f88e44ed8e3d245135bddf8798e8f81567
ybunnn-dev <belaroivan17@gmail.com>
2025-09-06 17:04:00 +0800

    appended activities in daily activities table

---
+++ database/migrations/2025_09_06_090231_add_activities_to_daily_activities_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('daily_activities', function (Blueprint $table) {
+            $table->string('activities')->default('No Activity')->after('icon_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('daily_activities', function (Blueprint $table) {
+            $table->dropColumn('activities');
+        });
+    }
+};
