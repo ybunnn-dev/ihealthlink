ae6d60018a4ab97e5d85ce85d28cbf7be73a9dd0
John Ivan Belaro <belaroivan17@gmail.com>
2025-11-07 21:14:05 +0800

    family fix

--- app/Http/Controllers/Web/FamilyController.php
+++ app/Http/Controllers/Web/FamilyController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 use App\Models\Barangay;
 use App\Models\Household;
