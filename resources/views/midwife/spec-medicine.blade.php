584cb086547dbcdf2f1d396a8648ae73061b404a
John Ivan Belaro <belaroivan17@gmail.com>
2025-11-15 22:14:35 +0800

    medicine yesssss

--- resources/views/midwife/spec-medicine.blade.php
+++ resources/views/midwife/spec-medicine.blade.php
@@ -86,41 +86,6 @@
                             <div class="pb-6">
                                 <!-- Flex container -->
                                 <div class="flex flex-col slg2:flex-row slg2:items-end gap-4">
-                                    <!-- Search bar -->
-                                    <div class="w-full slg2:w-64 slg2:flex-grow slg2:max-w-md">
-                                    <label for="default-search" class="mb-2 text-sm font-medium text-main_font">Search</label> 
-                                    <div class="relative">
-                                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                        </svg>
-                                        </div>
-                                        <input type="search" id="default-search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..."/>
-                                    </div>
-                                    </div> 
-                                    
-                                    <!-- Date Filter -->
-                                    <div class="w-full sm:w-48">
-                                        <label for="dateDropdown" class="mb-2 text-sm font-medium text-main_font">Sort By Date</label> 
-                                        <button id="dateDropdown" data-dropdown-toggle="dateDropdownMenu" class="w-full text-main_font bg-f7 focus:outline-none font-medium border border-navboard rounded-lg text-sm px-4 py-2 text-center inline-flex items-center justify-between h-[2.375rem]" type="button">
-                                        Date
-                                        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                                        </svg>
-                                        </button>
-                                    </div>
-
-                                     <div class="w-full sm:w-48">
-                                        <label for="statusDropdown" class="mb-2 text-sm font-medium text-main_font">Filter Status</label> 
-                                        <button id="statusDropdown" data-dropdown-toggle="statusDropdownMenu" class="w-full text-main_font bg-f7 focus:outline-none font-medium border border-navboard rounded-lg text-sm px-4 py-2 text-center inline-flex items-center justify-between h-[2.375rem]" type="button">
-                                        Select Status
-                                        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                                        </svg>
-                                        </button>
-                                    </div>
-
-                                    <!-- Add Medicine Button -->
                                     <div class="w-full xs:w-40 pt-5 xs:pt-0">
                                         <button type="button" id="add-batch-tigger" class="w-full h-[2.375rem] text-f7 bg-mainblue hover:text-mainblue hover:bg-nav_active font-medium rounded-lg text-sm px-3" >+   Add Batch</button>
                                     </div>
